2ba645fb3e2bf4095ef18d4b1442b77a3a49f624
unknown <raesupkim@gmail.com>
2021-10-20 13:58:11 -0600

    Data Transformation filter, reduce

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -173,6 +173,53 @@ const movementsDescriptions = movements.map((movement, i, array) => {
 console.log(movementsDescriptions);
 
 ///////////////////////////////////////
+//  Data Transformation: Filter
+const deposits = movements.filter(function(mov) {
+ return mov > 0;
+})
+console.log(movements);
+console.log(deposits);
+
+const withdrawls = movements.filter(mov => mov < 0);
+console.log(withdrawls);
+
+///////////////////////////////////////
+//  Data Transformation: Reduce
+const balance = movements.reduce(function(acc, cur, i, arr) {
+  console.log(`Iteration ${i}: ${acc}`)
+  return acc + cur;
+}, 0) //0 is a default value of acc.
+
+console.log(balance);
+
+const balance1 = movements.reduce((acc, mov) => acc + mov);
+console.log(balance1);
+
+let balance2 = 0;
+for (const mov of movements) {
+  balance2 += mov;
+}
+console.log(balance2);
+
+// Maximum value of the movements
+const max = movements.reduce((acc, mov) => acc < mov ? mov : acc)
+console.log(max);
+
+
+///////////////////////////////////////
+//  The Magic of Chaining Methods
+// const eurToUsd = 1.1;
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  .map((mov, i, arr) => {
+    // to check the previous step was correct or not
+    console.log(arr);
+    return mov * eurToUsd})
+  .reduce((acc, mov) => acc + mov, 0);
+
+  console.log(totalDepositsUSD);
+
+///////////////////////////////////////
 // Coding Challenge #1
 
 /* 
@@ -193,26 +240,85 @@ TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
 GOOD LUCK 😀
 */
 
-const dogsJulia = [3, 5, 2, 12, 7];
-const dogsKate = [4, 1, 15, 8, 3];
+// const dogsJulia = [3, 5, 2, 12, 7];
+// const dogsKate = [4, 1, 15, 8, 3];
 
-const checkDogs = function(dogsJulia, dogsKate) {
-  const dogsJuliaCorrected = dogsJulia.slice(1,-2);
-  const dogsKateCorrected = dogsKate.slice(1,-2);
+// const checkDogs = function(dogsJulia, dogsKate) {
+//   const dogsJuliaCorrected = dogsJulia.slice(1,-2);
+//   const dogsKateCorrected = dogsKate.slice(1,-2);
 
-  dogsJuliaCorrected.forEach((dog, i) => {
-    dog > 3 ? 
-    console.log(`Dog number ${i} is an adult, and is ${dog} years old`) :
-    console.log(`Dog number ${i} is still a puppy`)
-  })
+//   dogsJuliaCorrected.forEach((dog, i) => {
+//     dog > 3 ? 
+//     console.log(`Dog number ${i} is an adult, and is ${dog} years old`) :
+//     console.log(`Dog number ${i} is still a puppy`)
+//   })
 
-  dogsKateCorrected.forEach((dog, i) => {
-    dog > 3 ? 
-    console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`) :
-    console.log(`Dog number ${i + 1} is still a puppy`)
-  })
-  console.log(dogsJuliaCorrected);
-  console.log(dogsKateCorrected);
-}
+//   dogsKateCorrected.forEach((dog, i) => {
+//     dog > 3 ? 
+//     console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`) :
+//     console.log(`Dog number ${i + 1} is still a puppy`)
+//   })
+//   console.log(dogsJuliaCorrected);
+//   console.log(dogsKateCorrected);
+// }
 
-checkDogs(dogsJulia, dogsKate);
+// checkDogs(dogsJulia, dogsKate);
+
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+4. Run the function for both test datasets
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+// const calcAverageHumanAge = function(ages) {
+//   const adultDogsHumanAges = ages
+//     .map(age => age <= 2 ? age * 2 : age * 4 + 16)
+//     .filter(age => age >= 18);
+//     console.log(adultDogsHumanAges);
+
+//   const average = adultDogsHumanAges
+//     // .reduce((acc, age) => acc + age, 0) / adultDogsHumanAges.length;
+//     .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+//     return average;
+// }
+
+// console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+// console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
+
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an arrow function, and using chaining!
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+const calcAverageHumanAge = ages =>
+  ages
+    .map(age => age <= 2 ? age * 2 : 16 + age * 4)
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0)
+
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg1, avg2);
